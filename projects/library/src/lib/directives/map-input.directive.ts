93895b93b6903d5edd1d1a7943895f583d5f37db
Alex Sorafumo <alex@yuion.net>
2019-06-26 11:02:55 +1000

    fix: update listener handling and add lookup table for focus points

--- projects/library/src/lib/directives/map-input.directive.ts
+++ projects/library/src/lib/directives/map-input.directive.ts
@@ -1,15 +1,20 @@
-import { Directive, Input, Output, EventEmitter, HostListener, Renderer2, SimpleChanges, ElementRef, OnChanges } from '@angular/core';
+import { Directive, Input, Output, EventEmitter, HostListener, Renderer2, SimpleChanges, ElementRef, OnChanges } from "@angular/core";
+
 import { BaseWidgetComponent } from '../base.component';
+import { IMapPoint, IMapFeature } from 'library/public-api';
 import { MapService } from '../services/map.service';
 import { MapUtilities } from '../utlities/map.utilities';
-import { IMapPoint, IMapFeature } from '../components/map.interfaces';
-
 
 export interface IMapListener {
     id: string;
     selector: string;
     event: string;
     callback: Function;
+}
+
+interface POILocation {
+    scale: number;
+    center: IMapPoint;
 }
 
 @Directive({
@@ -20,7 +25,8 @@ export class MapInputDirective extends BaseWidgetComponent implements OnChanges 
     @Input() public center: IMapPoint;
     @Input() public listeners: IMapListener[];
     @Input() public focus: IMapFeature;
-    @Input() public map: Element;
+    @Input() public map: SVGElement;
+    @Input() public src: string;
     @Input() public lock: boolean;
     @Output() public scaleChange = new EventEmitter();
     @Output() public centerChange = new EventEmitter();
@@ -28,12 +34,22 @@ export class MapInputDirective extends BaseWidgetComponent implements OnChanges 
 
     private model: { [name: string]: any } = {};
 
+    private _active_listeners: (() => void)[] = [];
+
+    private map_box: ClientRect;
+    private lookup: { [src: string]: { [id: string]: POILocation } } = {};
+
     constructor(private el: ElementRef<HTMLElement>, private service: MapService, private renderer: Renderer2) {
         super();
     }
 
     public ngOnChanges(changes: SimpleChanges) {
         super.ngOnChanges(changes);
+        if (changes.src) {
+            if (!this.lookup[this.src]) {
+                this.lookup[this.src] = {};
+            }
+        }
         if (changes.map && this.map) {
             this.update();
         }
@@ -69,6 +85,14 @@ export class MapInputDirective extends BaseWidgetComponent implements OnChanges 
         this.model.listen_move_end = this.renderer.listen('window', 'mouseup', (e) => this.moveEnd());
     }
 
+    /**
+     * End move events on contextmenu action
+     * @param e
+     */
+    @HostListener('contextmenu', ['$event']) public onContext(e) {
+        this.timeout('contextmenu', () => this.moveEnd());
+    }
+
     public moveStart(e) {
         if (this.model.listen_move) {
             this.model.listen_move();
@@ -82,7 +106,6 @@ export class MapInputDirective extends BaseWidgetComponent implements OnChanges 
         this.model.target = e.target;
         this.model.dx = e.center.x;
         this.model.dy = e.center.y;
-        this.model.map_box = this.map.getBoundingClientRect();
     }
 
     public moveEnd() {
@@ -95,6 +118,7 @@ export class MapInputDirective extends BaseWidgetComponent implements OnChanges 
             this.model.listen_move_end = null;
         }
     }
+
 
     public get isIE() {
         return navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || !!navigator.userAgent.match(/MSIE/g);
@@ -110,8 +134,8 @@ export class MapInputDirective extends BaseWidgetComponent implements OnChanges 
         const dx = e.center.x - this.model.dx;
         const dy = e.center.y - this.model.dy;
         const center = this.model.center_start || { x: .5, y: .5 }
-        const delta_x = +(dx / this.model.map_box.width).toFixed(4);
-        const delta_y = +(dy / this.model.map_box.height).toFixed(4);
+        const delta_x = +(dx / this.map_box.width).toFixed(4);
+        const delta_y = +(dy / this.map_box.height).toFixed(4);
         this.model.center = { x: center.x - delta_x, y: center.y - delta_y };
         this.changePosition();
     }
@@ -130,7 +154,7 @@ export class MapInputDirective extends BaseWidgetComponent implements OnChanges 
     @HostListener('pinchstart', ['$event']) public pinchStart(e) {
         if (this.lock) { return; }
         this.model.dz = e.scale;
-        const box = this.model.map_box;
+        const box = this.map_box;
         const dist = Math.sqrt(box.width * box.width + box.height * box.height);
         const width = e.pointers[0].clientX - e.pointers[1].clientX;
         const height = e.pointers[0].clientY - e.pointers[1].clientY;
@@ -158,20 +182,32 @@ export class MapInputDirective extends BaseWidgetComponent implements OnChanges 
             if (!this.map) {
                 return this.timeout('focus', () => this.focusItem());
             }
+            if (!this.map_box) {
+                this.map_box = this.map.getBoundingClientRect();
+            }
             const selector = this.focus.id ? `#${MapUtilities.cleanCssSelector(this.focus.id)}` : ''
             const el = this.focus.id ? this.map.querySelector(selector) : null;
             if (el) { // Focus on element
-                const box = el.getBoundingClientRect();
-                const map_box = this.map.getBoundingClientRect();
-                this.model.center = {
-                    x:  ((box.left + box.width / 2) - map_box.left) / map_box.width,
-                    y:  ((box.top + box.height / 2) - map_box.top) / map_box.height
-                };
-                this.model.scale = this.focus.zoom ? this.focus.zoom / 100 : MapUtilities.getFillScale(map_box, box) * .6;
+                if (this.lookup[this.src][selector]) {
+                    this.model.center = this.lookup[this.src][selector].center;
+                    this.focus.zoom ? this.focus.zoom / 100 : this.lookup[this.src][selector].scale;
+                } else {
+                    const box = el.getBoundingClientRect();
+                    const map_box = this.map.getBoundingClientRect();
+                    this.model.center = {
+                        x:  ((box.left + box.width / 2) - map_box.left) / map_box.width,
+                        y:  ((box.top + box.height / 2) - map_box.top) / map_box.height
+                    };
+                    this.model.scale = this.focus.zoom ? this.focus.zoom / 100 : MapUtilities.getFillScale(map_box, box) * .6;
+                    this.lookup[this.src][selector] = {
+                        scale: MapUtilities.getFillScale(map_box, box) * .6,
+                        center: { ...this.model.center }
+                    };
+                }
                 this.changePosition(true)
             } else if (this.focus.coordinates) { // Focus on coordinates
                 const pnt = this.focus.coordinates;
-                const ratio = this.model.map_box.width / this.model.map_box.height;
+                const ratio = this.map_box.height / this.map_box.width;
                 this.model.center = { x: pnt.x / 10000, y: pnt.y / (10000 * ratio) };
                 this.model.scale = this.focus.zoom ? this.focus.zoom / 100 : 1;
                 this.changePosition(true);
@@ -184,8 +220,8 @@ export class MapInputDirective extends BaseWidgetComponent implements OnChanges 
      */
     public update() {
         if (this.map) {
-            this.model.map_box = this.map.getBoundingClientRect();
-            if (this.model.map_box.height === 0 || this.model.map_box.width === 0) {
+            this.map_box = this.map.getBoundingClientRect();
+            if (this.map_box.height === 0 || this.map_box.width === 0) {
                 return this.timeout('update_fail', () => this.update());
             }
             this.model.scale = 1;
@@ -238,9 +274,14 @@ export class MapInputDirective extends BaseWidgetComponent implements OnChanges 
                 if (el) {
                     this.renderer.setStyle(el, 'pointer-events', 'auto');
                     this.renderer.setStyle(el, 'display', 'inline-block');
-                    this.subs.obs[`listen_${selector}`] = this.renderer.listen(el, item.event || 'click', () => {
+                    const unsub = this.renderer.listen(el, item.event || 'click', () => {
                         if (item.callback) { item.callback(); }
                         else { this.event.emit({ id: selector, type: 'listener_event' }); }
+                    })
+                    this._active_listeners.push(() => {
+                        this.renderer.setStyle(el, 'pointer-events', '');
+                        this.renderer.setStyle(el, 'display', '');
+                        unsub();
                     });
                 } else {
                     this.service.log('Warn', `Unable to find element with selector '${item.id ? `#${item.id}` : `${item.selector}`}'`, item.id ? `#${item.id}` : `${item.selector}`)
@@ -254,19 +295,12 @@ export class MapInputDirective extends BaseWidgetComponent implements OnChanges 
      * Remove existing listeners from map
      */
     private clearListeners() {
-        if (this.model.listeners) {
-            for (const item of this.listeners) {
-                const selector = item.id ? `#${item.id}` : `${item.selector}`;
-                if (this.subs.obs[`listen_${selector}`]) {
-                    const el = this.map.querySelector(selector);
-                    if (el) {
-                        this.renderer.setStyle(el, 'pointer-events', '');
-                        this.renderer.setStyle(el, 'display', '');
-                    }
-                    const obs = this.subs.obs[`listen_${selector}`];
-                    obs instanceof Function ? obs() : obs.unsubscribe();
-                }
+        if (this._active_listeners) {
+            for (const listener of this._active_listeners) {
+                listener();
             }
+            delete this._active_listeners;
+            this._active_listeners = [];
         }
     }
 
